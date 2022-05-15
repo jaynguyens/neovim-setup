90fbdccdadd5e156abfbb4eb7f57596b33dcfe0e
Jay Nguyen <jay@nguyens.co>
2022-05-15 16:51:49 +0100

    .

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,7 +11,8 @@ require('telescope').setup {
         -- e.g. git_{create, delete, ...}_branch for the git_branches picker
         ["<C-h>"] = "which_key"
       }
-    }
+    },
+    file_ignore_patterns = { "node_modules" }
   },
   extensions = {
     fzf = {
@@ -19,9 +20,8 @@ require('telescope').setup {
       override_generic_sorter = true,  -- override the generic sorter
       override_file_sorter = true,     -- override the file sorter
       case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
     }
-  }
+  },
 }
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
